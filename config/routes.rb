270c4fa3a2f858db41e5932fbde435dd6ead042f
omar sehgal <omar.sehgal@gmail.com>
2011-05-06 14:30:09 -0700

    new changes committed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 SuperApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+
+  get "users/new"
+
+  match '/' => 'pages#home'
+  match '/help' => 'pages#help'
+  match '/signin' => 'pages#signin'
+  match '/signup' => 'users#new'
+  
+  match '/about' => 'pages#about'
+  match '/contact' => 'pages#contact'
+
+  match '/signout' => 'pages#signout'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +61,7 @@ SuperApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
